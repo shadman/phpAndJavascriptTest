8bfbb87c59305024dace8c1b2f9add33528a78c2
Shadman Jamil <shadman.jamil@gmail.com>
2016-02-14 20:25:53 +0500

    added services and updated static carousel to dynamic

--- client/js/script.js
+++ client/js/script.js
@@ -1,85 +1,24 @@
 var app = angular.module("slick-app", ["slick"]);
 
-app.controller('appController', function($scope, $timeout){
+app.controller('appController', function($scope, $timeout, $http){
   
   $scope.horizontal_items = [];
   $scope.vertical_items = [];
-   
-	$scope.horizontal_items = [
-	  {
-		src: 'http://mena-cdn-lb.aws.playco.com/Starz/BLACKSAILSY2015S03E001/BLACKSAILSY2015S03E001-1536x613-DMHE.jpg',
-		title: 'label 1',
-		description: 'additional information here'
-	  },
-	  {
-		src: 'http://mena-cdn-lb.aws.playco.com/FOX/DAVINCISDEMONSY2013S02E007/DAVINCISDEMONSY2013S02E007-1536x613-DMHE.jpg',
-		title: 'label 2',
-		description: 'additional information here'
-	  },
-	  {
-		src: 'http://mena-cdn-lb.aws.playco.com/Sony/OUTLANDERY2014S01E009/OUTLANDERY2014S01E009-1536x613-DMHE.jpg',
-		title: 'label 3',
-		description: 'additional information here'
-	  },
-	  {
-		src: 'http://mena-cdn-lb.aws.playco.com/CBS/JANETHEVIRGINY2014S01E009/JANETHEVIRGINY2014S01E009-1536x613-DMHE.jpg',
-		title: 'label 4',
-		description: 'additional information here'
-	  },
-	  {
-		src: 'http://mena-cdn-lb.aws.playco.com/FOX/DAVINCISDEMONSY2013S02E007/DAVINCISDEMONSY2013S02E007-1536x613-DMHE.jpg',
-		title: 'label 2',
-		description: 'additional information here'
-	  },
-	  {
-		src: 'http://mena-cdn-lb.aws.playco.com/CBS/JANETHEVIRGINY2014S01E009/JANETHEVIRGINY2014S01E009-1536x613-DMHE.jpg',
-		title: 'label 4',
-		description: 'additional information here'
-	  },
-	  {
-		src: 'http://mena-cdn-lb.aws.playco.com/CBS/JANETHEVIRGINY2014S01E009/JANETHEVIRGINY2014S01E009-1536x613-DMHE.jpg',
-		title: 'label 4',
-		description: 'additional information here'
-	  },
-	];
+  $scope.results = [];
 
-
-	$scope.vertical_items = [
-	  {
-		src: 'http://mena-cdn-lb.aws.playco.com/Sony/SLEEPLESSINSEATTLEY1993M/SLEEPLESSINSEATTLEY1993M-474x677-PST.jpg',
-		title: 'label 1',
-		description: 'additional information here, additional information here, additional information here, additional information here.'
-	  },
-	  {
-		src: 'http://mena-cdn-lb.aws.playco.com/Miramax/CHOCOLATY2000M/CHOCOLATY2000M-474x677-PST.jpg',
-		title: 'label 2'
-	  },
-	  {
-		src: 'http://mena-cdn-lb.aws.playco.com/WarnerBrothers/THETIMETRAVELERSWIFEY2009MSUB/THETIMETRAVELERSWIFEY2009MSUB-474x677-PST.jpg',
-		title: 'label 3',
-		description: 'additional information here'
-	  },
-	  {
-		src: 'http://mena-cdn-lb.aws.playco.com/Miramax/SERENDIPITYY2001M/SERENDIPITYY2001M-474x677-PST.jpg',
-		title: 'label 4',
-		description: 'additional information here'
-	  },
-	  {
-		src: 'http://mena-cdn-lb.aws.playco.com/Lionsgate/DIRTYDANCINGY1987M/DIRTYDANCINGY1987M-474x677-PST.jpg',
-		title: 'label 5',
-		description: 'additional information here'
-	  },
-	  {
-		src: 'http://mena-cdn-lb.aws.playco.com/Sony/AUSTENLANDY2013M/AUSTENLANDY2013M-474x677-PST.jpg',
-		title: 'label 6',
-		description: 'additional information here'
-	  },
-	  {
-		src: 'http://mena-cdn-lb.aws.playco.com/Miramax/KATEANDLEOPOLDY2001M/KATEANDLEOPOLDY2001M-474x677-PST.jpg',
-		title: 'label 7',
-		description: 'additional information here'
-	  },
-	];
-    
-	$scope.dataLoaded = true;
+  $timeout(function(){
+	$http.get('http://localhost/starzplay/phpAndJavascriptTest/services/web/app_dev.php/carousel').success(function(data) {
+		$scope.results = data.result;
+			
+		$scope.horizontal_items = $scope.results.horizontalCarousel;
+		$scope.vertical_items = $scope.results.verticalCarousel;
+			
+		$scope.dataLoaded = true;
+		console.log("request success.");
+		
+	}).error(function (data) {
+		console.log("request failed.");
+	});
+  }, 0);
+	
 });
